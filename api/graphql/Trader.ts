581074b1fe46a84baa03893c827f08ff9b6aef2d
Chris Head <c.head@shell.com>
2023-01-16 16:27:25 +0000

    feat: added basic Trader resolvers

---
+++ api/graphql/Trader.ts
@@ -0,0 +1,45 @@
+import { ApolloError } from "apollo-server";
+import { arg, extendType, list, nonNull, objectType } from "nexus";
+
+export const Trader = objectType({
+  name: "Trader",
+  definition(t) {
+    t.nonNull.id("id");
+    t.nonNull.string("name");
+  },
+});
+
+export const TraderQuery = extendType({
+  type: "Query",
+  definition(t) {
+    t.field("traders", {
+      type: nonNull(list(nonNull("Trader"))),
+      resolve() {
+        return [
+          {
+            id: "1",
+            name: "Barry Chuckle",
+          },
+        ];
+      },
+    });
+    t.field("trader", {
+      type: "Trader",
+      args: {
+        id: arg({
+          description: "The ID of the trader",
+          type: nonNull("ID"),
+        }),
+      },
+      resolve(_, { id }) {
+        if (id !== "1") {
+          throw new ApolloError("trader not found");
+        }
+        return {
+          id: "1",
+          name: "Barry Chuckle",
+        };
+      },
+    });
+  },
+});
