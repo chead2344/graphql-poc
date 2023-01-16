5768f158114cfc970b7f59acd21796912b6e7112
Chris Head <c.head@shell.com>
2023-01-16 18:13:41 +0000

    docs: added GraphQL docs

--- api/context.ts
+++ api/context.ts
@@ -1,5 +1,5 @@
 import { BlobServiceClient } from "@azure/storage-blob";
-import { blobContainerName, blobName, blobConnectionString } from "./config";
+import { blobConnectionString, blobContainerName, blobName } from "./config";
 import TradersDataSource from "./datasources/TradersDataSource";
 import { server } from "./server";
 
@@ -13,10 +13,8 @@ const blobClient = BlobServiceClient.fromConnectionString(blobConnectionString)
   .getContainerClient(blobContainerName)
   .getBlobClient(blobName);
 
-export const context = async (): Promise<Context> => {
-  return {
-    dataSources: {
-      tradersAPI: new TradersDataSource(server.cache, blobClient),
-    },
-  };
-};
+export const context = async (): Promise<Context> => ({
+  dataSources: {
+    tradersAPI: new TradersDataSource(server.cache, blobClient),
+  },
+});
--- api/graphql/Trader.ts
+++ api/graphql/Trader.ts
@@ -4,16 +4,25 @@ import { arg, extendType, list, nonNull, objectType } from "nexus";
 export const Trader = objectType({
   name: "Trader",
   definition(t) {
-    t.nonNull.string("countryCode");
-    t.nonNull.string("email");
-    t.list.string("entitlements");
+    t.nonNull.string("countryCode", {
+      description: "The country the trader operates in",
+    });
+    t.nonNull.string("email", {
+      description: "The email address of the trader",
+    });
+    t.list.string("entitlements", {
+      description:
+        "The permissions associated to the trader, comprised of business entity, role and team",
+    });
   },
 });
 
 export const TraderQuery = extendType({
   type: "Query",
   definition(t) {
+    // TODO: Add pagination
     t.field("traders", {
+      description: "Returns all traders",
       type: nonNull(list(nonNull("Trader"))),
       resolve(_source, _args, { dataSources }) {
         return dataSources.tradersAPI.getAll();
@@ -21,6 +30,7 @@ export const TraderQuery = extendType({
     });
     t.field("trader", {
       type: "Trader",
+      description: "Returns a single trader by their email address",
       args: {
         email: arg({
           description: "The email of the trader",
